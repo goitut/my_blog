4ed8bc19ace9efe43e62615ee58b62e1a5dc4eee
goitut <goitutriay@gmail.com>
2025-10-24 01:28:37 +0300

    adding new blogs to database.

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
   path('', index, name='index'),
   path('about/', about, name='about'),
   path('contact/', contact, name='contact'),
+  path('blogs/', blog_list, name='blog_list'),
 ]
